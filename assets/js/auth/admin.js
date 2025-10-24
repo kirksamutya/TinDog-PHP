04f22ad75ed8f5234a11bd3ac292c03f9a029dfc
Anthony Saavedra <AnthonySaavedra@protonmail.com>
2025-10-24 15:02:17 +0800

    API and Database moved to Laravel and Supabase

--- assets/js/auth/admin.js
+++ assets/js/auth/admin.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const errorAlert = document.getElementById("login-error-alert");
     errorAlert.style.display = "none";
 
-    fetch(getBasePath() + "api/admin-login.php", {
+    fetch("http://127.0.0.1:8000/api/admin-login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
